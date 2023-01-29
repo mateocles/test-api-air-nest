549e52150d2acbfb33f0dd67d50db7f7fe6e4a96
mateocles <mat.bermudez@udla.edu.co>
2023-01-29 01:55:54 -0500

    add: model user and connection db mongose

--- src/common/util/cryptp.service.ts
+++
@@ -1,8 +0,0 @@
-import * as crypto from 'crypto';
-
-export class CryptoService {
-  encrypt(text) {
-    const key = crypto.pbkdf2Sync(text, 'salt', 10000, 64, 'sha512');
-    return key.toString('hex');
-  }
-}
