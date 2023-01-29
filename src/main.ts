a0eaff170b9793b9efd3de96df4b3bc185867186
mateocles <mat.bermudez@udla.edu.co>
2023-01-29 01:25:02 -0500

    add: interceptor timeout

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filter/http-exception.filter';
+import { TimeoutOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeoutOutInterceptor());
   await app.listen(3000);
 }
 bootstrap();
