babcb0ed41f3c33b810d08372479a72e99078a85
mateocles <mat.bermudez@udla.edu.co>
2023-01-29 02:17:30 -0500

    add: endpoint create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
-@Controller('user')
-export class UserController {}
+import { UserDTO } from './dto/user.dto';
+import { UserService } from './user.service';
+
+@Controller('api/v1/user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() userDTO: UserDTO) {
+    return this.userService.create(userDTO);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { IUser } from 'src/common/interfaces/user.interface';
+import { UserDTO } from './dto/user.dto';
+import { USER } from 'src/common/models/models';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(USER.name) private readonly model: Model<IUser>) {}
+
+  async hasPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+  }
+
+  async create(userDTO: UserDTO): Promise<IUser> {
+    const hash = await this.hasPassword(userDTO.password);
+    const newUser = new this.model({ ...userDTO, password: hash });
+    return await newUser.save();
+  }
+}
